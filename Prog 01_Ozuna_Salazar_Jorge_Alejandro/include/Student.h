5d799c2e97f3d724c1a3c596233f4d8aeab7a659
Jorge Ozuna <122687128+TRIYAN-SALAZAR@users.noreply.github.com>
2026-01-30 21:34:01 -0600

    Refactor Student class: remove default constructor, add averageGrade member variable, and update constructor definitions

--- Prog 01_Ozuna_Salazar_Jorge_Alejandro/include/Student.h
+++ Prog 01_Ozuna_Salazar_Jorge_Alejandro/include/Student.h
@@ -1,16 +1,24 @@
 #ifndef STUDENT_H
 #define STUDENT_H
-
+#include <iostream>
 
 class Student
 {
     public:
-        Student();
+        std::string name;
+        int ID;
+        float averageGrade;
+        Student(std::string, int);
+        Student(std::string, int, float);
+        
         ~Student();
 
-    protected:
-
-    private:
+        void showStudentData();
+        void setUpdateAverageGrade(int);
+        void setUpdateName(std::string);
+        
+        bool doesTheStudenPass();
+        bool isTheStudenoutstanding();
 };
 
 #endif // STUDENT_H
--- Prog 01_Ozuna_Salazar_Jorge_Alejandro/src/Student.cpp
+++ Prog 01_Ozuna_Salazar_Jorge_Alejandro/src/Student.cpp
@@ -1,11 +1,31 @@
 #include "Student.h"
+#include <iostream>
 
-Student::Student()
-{
-    //ctor
+Student::Student(std::string name, int studenID, float averageGrade) : name(name), ID(studenID) {}
+Student::Student(std::string name, int studenID, float averageGrade) : name(name), ID(studenID), averageGrade(averageGrade) {}
+
+Student::~Student() {}
+
+void Student::showStudentData() {
+    std::cout << "Name: " << this->name << std::endl;
+    std::cout << "Student ID: " << this->ID << std::endl;
+    std::cout << "Average Grade: " << this->averageGrade << std::endl;
 }
 
-Student::~Student()
-{
-    //dtor
+void Student::setUpdateAverageGrade(int average) {
+    this->averageGrade = average;
+}
+
+void Student::setUpdateName(std::string name) {
+    this->name = name;
+}
+
+bool Student::doesTheStudenPass() {
+    if(this->averageGrade >= 6.0) return true;
+    return false;
+}
+
+bool Student::isTheStudenoutstanding() {
+    if(this->averageGrade >= 9.0) return true;
+    return false;
 }
