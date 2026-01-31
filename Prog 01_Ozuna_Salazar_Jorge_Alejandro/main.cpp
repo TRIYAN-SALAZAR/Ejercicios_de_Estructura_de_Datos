23f3dc6b0194f58224e03b93b9af5e9d8e274825
Jorge Ozuna <122687128+TRIYAN-SALAZAR@users.noreply.github.com>
2026-01-30 22:01:27 -0600

    Refactor main function: enhance course and student information display with structured output

--- Prog 01_Ozuna_Salazar_Jorge_Alejandro/main.cpp
+++ Prog 01_Ozuna_Salazar_Jorge_Alejandro/main.cpp
@@ -7,19 +7,24 @@ int main()
     Course crs1("Data Structures", 100, 6);
     Course crs2("Databases", 101, 4);
 
-    std::cout << "credits of DSA: " << (crs1.getCredits()) << std::endl;
+    std::cout << "==================== Instancia 1 - Estructura de datos ====================" << std::endl;
+    std::cout << "Creditos de Estructura de Datos: " << (crs1.getCredits()) << std::endl;
     crs1.setChangeName("Data Structures");
     crs1.setCredits(5);
     crs1.showCourseInformation();
+    std::cout << std::endl;
 
-    std::cout << "credits of DSA: " << (crs2.getCredits()) << std::endl;
+    std::cout << "==================== Instancia 2 - Base de Datos ====================" << std::endl;
+    std::cout << "Creditos de Bases de datos: " << (crs2.getCredits()) << std::endl;
     crs2.setChangeName("Data Structures");
     crs2.setCredits(5);
     crs2.showCourseInformation();
+    std::cout << std::endl;
 
     Student A("Jorge Ozuna", 10001);
     Student B("Oscar Khay", 10002);
-
+    
+    std::cout << "==================== Instancia 1 - Estudiante A ====================" << std::endl;
     A.setUpdateAverageGrade(7.4f);
     A.setUpdateName("Jorge Ozuna Salazar");
     if(A.doesTheStudenPass())
@@ -32,7 +37,9 @@ int main()
     else
         std::cout << "El estudiante no es sobresaliente :(" << std::endl;
     A.showStudentData();
+    std::cout << std::endl;
 
+    std::cout << "==================== Instancia 1 - Estudiante B ====================" << std::endl;
     B.setUpdateAverageGrade(9.1f);
     B.setUpdateName("Oscar Khay Ray");
     if(B.doesTheStudenPass())
