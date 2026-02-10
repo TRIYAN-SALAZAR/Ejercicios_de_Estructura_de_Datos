a17c09b2dfd362babd5de65da608037ee5b7cd04
Jorge Ozuna <122687128+TRIYAN-SALAZAR@users.noreply.github.com>
2026-02-09 23:07:41 -0600

    Refactor main function to implement linked list operations for insertion, deletion, and display

--- PRACTICAS/simple_linked_list/main.cpp
+++ PRACTICAS/simple_linked_list/main.cpp
@@ -1,9 +1,34 @@
 #include <iostream>
+#include "SimpleList.h"
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    int dato;
+
+    SimpleList *list = new SimpleList();
+
+    list->insertarInicio(1);
+    list->insertarInicio(2);
+    list->insertarInicio(3);
+    list->insertarInicio(4);
+
+    list->insertarFinal(5);    
+    list->insertarFinal(6);
+    list->insertarFinal(7);
+
+    list->mostrarTodo();
+    
+    cout << endl << endl;
+    cout << "Dato a eliminar: ";
+    cin  >> dato;
+    
+    while(getchar() != '\n');
+    
+    list->eliminar(dato);
+    list->eliminar(5);
+    list->mostrarTodo();
+
     return 0;
 }
