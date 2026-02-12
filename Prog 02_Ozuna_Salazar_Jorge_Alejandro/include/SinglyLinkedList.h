e0687ee4dd0e68f5fee4fa1a44ba1565843d448b
Jorge Ozuna <122687128+TRIYAN-SALAZAR@users.noreply.github.com>
2026-02-12 17:35:05 -0600

    Add initial implementation of Nodo and SinglyLinkedList classes with basic constructors and destructors

---
+++ Prog 02_Ozuna_Salazar_Jorge_Alejandro/include/SinglyLinkedList.h
@@ -0,0 +1,16 @@
+#ifndef SINGLYLINKEDLIST_H
+#define SINGLYLINKEDLIST_H
+
+
+class SinglyLinkedList
+{
+    public:
+        SinglyLinkedList();
+        virtual ~SinglyLinkedList();
+
+    protected:
+
+    private:
+};
+
+#endif // SINGLYLINKEDLIST_H
---
+++ Prog 02_Ozuna_Salazar_Jorge_Alejandro/src/SinglyLinkedList.cpp
@@ -0,0 +1,11 @@
+#include "SinglyLinkedList.h"
+
+SinglyLinkedList::SinglyLinkedList()
+{
+    //ctor
+}
+
+SinglyLinkedList::~SinglyLinkedList()
+{
+    //dtor
+}
