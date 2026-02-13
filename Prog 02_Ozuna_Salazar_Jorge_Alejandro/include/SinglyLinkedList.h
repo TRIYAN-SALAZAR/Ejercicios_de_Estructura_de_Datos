d6282c889614077c9b3c587caec019db37ae50d6
Jorge Ozuna <122687128+TRIYAN-SALAZAR@users.noreply.github.com>
2026-02-12 18:08:22 -0600

    Refactor SinglyLinkedList class to include Node pointer and update method visibility

--- Prog 02_Ozuna_Salazar_Jorge_Alejandro/include/SinglyLinkedList.h
+++ Prog 02_Ozuna_Salazar_Jorge_Alejandro/include/SinglyLinkedList.h
@@ -1,16 +1,30 @@
 #ifndef SINGLYLINKEDLIST_H
 #define SINGLYLINKEDLIST_H
 
+#include <Node.h>
 
 class SinglyLinkedList
 {
     public:
+        Node *head;
         SinglyLinkedList();
+        SinglyLinkedList(Node*);
         virtual ~SinglyLinkedList();
 
-    protected:
+        void insert_at_the_beggining(int);
+        void insert_at_the_end(int);
+        void insert_in_a_position(int);
 
-    private:
+        void search(int);
+        void delete_one(int);
+        void delete_all();
+
+        bool is_emty();
+        void first();
+        void last();
+        void previous_node();
+        void next_node();
+        void size();
 };
 
 #endif // SINGLYLINKEDLIST_H
