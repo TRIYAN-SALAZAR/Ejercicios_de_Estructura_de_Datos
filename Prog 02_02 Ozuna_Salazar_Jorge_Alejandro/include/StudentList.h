bb86f01e2efbbde9fd12bc6de6f50f30086cbc43
Jorge Ozuna <122687128+TRIYAN-SALAZAR@users.noreply.github.com>
2026-02-18 13:36:19 -0600

    New Project Singly Linked List of Students

---
+++ Prog 02_02 Ozuna_Salazar_Jorge_Alejandro/include/StudentList.h
@@ -0,0 +1,16 @@
+#ifndef STUDENTLIST_H
+#define STUDENTLIST_H
+
+
+class StudentList
+{
+    public:
+        StudentList();
+        virtual ~StudentList();
+
+    protected:
+
+    private:
+};
+
+#endif // STUDENTLIST_H
