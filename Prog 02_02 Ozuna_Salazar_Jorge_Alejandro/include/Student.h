31b3a7cb2b45ee7c7b4f7538546a75257b15f07d
Jorge Ozuna <122687128+TRIYAN-SALAZAR@users.noreply.github.com>
2026-02-18 21:11:01 -0600

    Refactor Student class: organize member variables and update constructor initialization

--- Prog 02_02 Ozuna_Salazar_Jorge_Alejandro/include/Student.h
+++ Prog 02_02 Ozuna_Salazar_Jorge_Alejandro/include/Student.h
@@ -1,16 +1,33 @@
 #ifndef STUDENT_H
 #define STUDENT_H
 
+#include <iostream>
 
 class Student
 {
+    private:
+        int age;
+        int grade;
+        float average;
+        std::string first_name;
+        std::string last_name;
+
     public:
         Student();
-        virtual ~Student();
-
-    protected:
-
-    private:
+        Student(int, std::string, std::string);
+        Student(int, int, float, std::string, std::string);
+        ~Student();
+        void set_age(int);
+        void set_grade(int);
+        void set_average(float);
+        void set_firstname(std::string);
+        void set_lastname(std::string);
+        
+        int get_age() const;
+        int get_grade() const;
+        float get_average() const;
+        std::string get_firstname() const;
+        std::string get_lastname() const;
 };
 
 #endif // STUDENT_H
--- Prog 02_02 Ozuna_Salazar_Jorge_Alejandro/src/Student.cpp
+++ Prog 02_02 Ozuna_Salazar_Jorge_Alejandro/src/Student.cpp
@@ -1,11 +1,25 @@
 #include "Student.h"
+#include <iostream>
 
-Student::Student()
-{
-    //ctor
-}
+using std::string;
+
+Student::Student() : age(0), grade(0), average(0.0f), first_name(""), last_name("") {}
+Student::Student(int age, string f_name, string l_name) {}
+Student::Student(int age, int grd, float avg, string f_name, string l_name) {}
 
 Student::~Student()
 {
     //dtor
 }
+
+void Student::set_age(int age) {}
+void Student::set_grade(int grd) {}
+void Student::set_average(float avg) {}
+void Student::set_firsname(std::string f_name) {}
+void Student::set_lastname(std::string l_name) {}
+
+int Student::get_age() const {}
+int Student::get_grade() const {}
+float Student::get_average() const {}
+string Student::get_firsname() const {}
+string Student::get_lastname() const {}
