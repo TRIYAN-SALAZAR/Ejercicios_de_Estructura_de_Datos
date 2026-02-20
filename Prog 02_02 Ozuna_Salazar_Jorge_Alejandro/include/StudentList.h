6089c3e0967e47102bc9720c32cbc3511dca4122
Jorge Ozuna <122687128+TRIYAN-SALAZAR@users.noreply.github.com>
2026-02-19 23:36:56 -0600

    Refactor StudentList class: reorganize member functions and improve formatting for better readability

--- Prog 02_02 Ozuna_Salazar_Jorge_Alejandro/include/StudentList.h
+++ Prog 02_02 Ozuna_Salazar_Jorge_Alejandro/include/StudentList.h
@@ -1,16 +1,55 @@
 #ifndef STUDENTLIST_H
 #define STUDENTLIST_H
 
+#include "Node.h"
+#include "Student.h"
 
-class StudentList
-{
+class StudentList {
+    private:
+        Node *head;
+        Node *current_position;
+        Node *previous;
+        int total_nodes;
+
     public:
         StudentList();
+        StudentList(Student*); 
+        StudentList(const StudentList& other);
+        StudentList& operator=(const StudentList& other);
         virtual ~StudentList();
 
-    protected:
+        void insert_at_the_beginning(Student*);
+        void insert_at_the_end(Student*);
+        void insert_at_position(Student*, int);
+        
+        void insert_at_the_beginning(int, int, float, const std::string&, const std::string&);
+        void insert_at_the_end(int , int, float, const std::string&, const std::string&);
 
-    private:
+        Node* search_by_name(const std::string&, const std::string&);
+        Node* search_by_age(int);
+        Node* search_by_grade(int);
+        Node* search_at_position(int);
+        bool contains(const std::string&, const std::string&);
+
+        bool delete_at_position(int);
+        bool delete_by_name(const std::string&, const std::string&);
+        bool delete_first();
+        void delete_all();
+
+        void show_all() const;
+        void show_at_position(int) const;
+        void show_reverse() const;
+
+        bool is_empty() const;
+        int size() const;
+        
+        Node* first();
+        Node* last();
+        Node* get_current();
+        Node* next_node();
+        Node* previous_node();
+        void reset_current();
+
 };
 
 #endif // STUDENTLIST_H
