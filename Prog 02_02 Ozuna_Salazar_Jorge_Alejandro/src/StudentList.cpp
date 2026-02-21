d7e212c4338cedec50bb5ece510a811b651c9d39
Jorge Ozuna <122687128+TRIYAN-SALAZAR@users.noreply.github.com>
2026-02-20 22:04:03 -0600

    Refactor StudentList constructors and assignment operator for improved clarity and memory management

--- Prog 02_02 Ozuna_Salazar_Jorge_Alejandro/src/StudentList.cpp
+++ Prog 02_02 Ozuna_Salazar_Jorge_Alejandro/src/StudentList.cpp
@@ -1,11 +1,63 @@
 #include "StudentList.h"
 
-StudentList::StudentList()
-{
-    //ctor
+StudentList::StudentList() : head(nullptr), current_position(nullptr), previous(nullptr), total_nodes(0) {}
+
+StudentList::StudentList(Student* student) : head(nullptr), current_position(nullptr), previous(nullptr), total_nodes(0) {
+    if(student != nullptr) {
+        head = new Node(student);
+        current_position = head;
+        total_nodes = 1;
+    }
 }
 
-StudentList::~StudentList()
-{
-    //dtor
+StudentList::StudentList(const StudentList& other) : head(nullptr), current_position(nullptr), previous(nullptr), total_nodes(0) {
+    if (other.head == nullptr) {
+        return;
+    }
+
+    head = new Node(new Student(*other.head->getData()));
+    total_nodes = 1;
+
+    Node* current_other = other.head->getNext();
+    Node* current_this = head;
+
+    while (current_other != nullptr) {
+        Node* new_node = new Node(new Student(*current_other->getData()));
+        current_this->setNext(new_node);
+        current_this = new_node;
+        current_other = current_other->getNext();
+        total_nodes++;
+    }
+}
+
+StudentList& StudentList::operator=(const StudentList& other) {
+    if (this != &other) {
+        // Limpiar la lista actual
+        delete_all();
+
+        if (other.head != nullptr) {
+            // Copiar el primer nodo
+            head = new Node(new Student(*other.head->getData()));
+            total_nodes = 1;
+
+            // Copiar el resto de nodos
+            Node* current_other = other.head->getNext();
+            Node* current_this = head;
+
+            while (current_other != nullptr) {
+                Node* new_node = new Node(new Student(*current_other->getData()));
+                current_this->setNext(new_node);
+                current_this = new_node;
+                current_other = current_other->getNext();
+                total_nodes++;
+            }
+        }
+    }
+    return *this;
+}
+
+
+StudentList::~StudentList() {
+    if(!is_empty()) 
+        delete_all();
 }
