90b6dd38b2f51da75ff52f31c400e1840134156a
Jorge Ozuna <122687128+TRIYAN-SALAZAR@users.noreply.github.com>
2026-02-21 21:14:45 -0600

    Refactor StudentList class: add tail variable and update node linking for doubly linked list

--- Prog 03_Ozuna_Salazar_Jorge_Alejandro/src/StudentList.cpp
+++ Prog 03_Ozuna_Salazar_Jorge_Alejandro/src/StudentList.cpp
@@ -4,31 +4,33 @@
 using std::cout;
 using std::endl;
 
-StudentList::StudentList() : head(nullptr), current_position(nullptr), previous(nullptr), total_nodes(0) {}
+StudentList::StudentList()
+    : head(nullptr), tail(nullptr), current_position(nullptr), total_nodes(0) {}
 
-StudentList::StudentList(Student* student) : head(nullptr), current_position(nullptr), previous(nullptr), total_nodes(0) {
-    if(student != nullptr) {
+StudentList::StudentList(Student* student)
+    : head(nullptr), tail(nullptr), current_position(nullptr), total_nodes(0) {
+    if (student != nullptr) {
         head = new Node(student);
+        tail = head;
         current_position = head;
         total_nodes = 1;
     }
 }
 
-StudentList::StudentList(const StudentList& other) : head(nullptr), current_position(nullptr), previous(nullptr), total_nodes(0) {
-    if (other.head == nullptr) {
-        return;
-    }
+StudentList::StudentList(const StudentList& other)
+    : head(nullptr), tail(nullptr), current_position(nullptr), total_nodes(0) {
+    if (other.head == nullptr) return;
 
     head = new Node(new Student(*other.head->getData()));
+    tail = head;
     total_nodes = 1;
 
     Node* current_other = other.head->getNext();
-    Node* current_this = head;
-
     while (current_other != nullptr) {
         Node* new_node = new Node(new Student(*current_other->getData()));
-        current_this->setNext(new_node);
-        current_this = new_node;
+        new_node->setPrev(tail);
+        tail->setNext(new_node);
+        tail = new_node;
         current_other = current_other->getNext();
         total_nodes++;
     }
@@ -36,22 +38,19 @@ StudentList::StudentList(const StudentList& other) : head(nullptr), current_posi
 
 StudentList& StudentList::operator=(const StudentList& other) {
     if (this != &other) {
-        // Limpiar la lista actual
         delete_all();
 
         if (other.head != nullptr) {
-            // Copiar el primer nodo
             head = new Node(new Student(*other.head->getData()));
+            tail = head;
             total_nodes = 1;
 
-            // Copiar el resto de nodos
             Node* current_other = other.head->getNext();
-            Node* current_this = head;
-
             while (current_other != nullptr) {
                 Node* new_node = new Node(new Student(*current_other->getData()));
-                current_this->setNext(new_node);
-                current_this = new_node;
+                new_node->setPrev(tail);
+                tail->setNext(new_node);
+                tail = new_node;
                 current_other = current_other->getNext();
                 total_nodes++;
             }
@@ -61,7 +60,7 @@ StudentList& StudentList::operator=(const StudentList& other) {
 }
 
 StudentList::~StudentList() {
-    if(!is_empty()) 
+    if (!is_empty())
         delete_all();
 }
 
@@ -73,10 +72,16 @@ void StudentList::insert_at_the_beginning(Student* student) {
 
     Node* temp = new Node(student);
     temp->setNext(head);
+
+    if (head != nullptr) {
+        head->setPrev(temp);   // el antiguo head ahora apunta atras
+    } else {
+        tail = temp;           // lista vacía: tail = head = nuevo nodo
+    }
+
     head = temp;
     total_nodes++;
 }
-
 
 void StudentList::insert_at_the_end(Student* student) {
     if (student == nullptr) {
@@ -86,14 +91,13 @@ void StudentList::insert_at_the_end(Student* student) {
 
     Node* temp = new Node(student);
 
-    if (head == nullptr) {
+    if (tail == nullptr) {
         head = temp;
+        tail = temp;
     } else {
-        Node* aux = head;
-        while (aux->getNext() != nullptr) {
-            aux = aux->getNext();
-        }
-        aux->setNext(temp);
+        temp->setPrev(tail);
+        tail->setNext(temp);
+        tail = temp;
     }
 
     total_nodes++;
@@ -115,6 +119,7 @@ void StudentList::insert_at_position(Student* student, int position) {
         return;
     }
 
+    // Llegar al nodo en position-1
     Node* aux = head;
     int current_pos = 0;
 
@@ -123,17 +128,33 @@ void StudentList::insert_at_position(Student* student, int position) {
         current_pos++;
     }
 
+    // Si aux es el ultimo nodo o hemos llegado al final, insertar al final
+    if (aux->getNext() == nullptr) {
+        insert_at_the_end(student);
+        return;
+    }
+
+    // Insertar entre aux y aux->getNext()
     Node* temp = new Node(student);
-    temp->setNext(aux->getNext());
+    Node* next_node = aux->getNext();
+
+    temp->setNext(next_node);
+    temp->setPrev(aux);
     aux->setNext(temp);
+    if (next_node != nullptr) {
+        next_node->setPrev(temp);
+    } else {
+        tail = temp;
+    }
+
     total_nodes++;
 
-    cout << "Estudiante " << student->get_firstname() << " " 
-         << student->get_lastname() << " insertado en posicion " 
+    cout << "Estudiante " << student->get_firstname() << " "
+         << student->get_lastname() << " insertado en posicion "
          << position << endl;
 }
 
-Node* StudentList::search_by_name(const std::string& first_name, 
+Node* StudentList::search_by_name(const std::string& first_name,
                                    const std::string& last_name) {
     if (head == nullptr) {
         cout << "Lista vacia" << endl;
@@ -143,15 +164,15 @@ Node* StudentList::search_by_name(const std::string& first_name,
     Node* aux = head;
     while (aux != nullptr) {
         Student* student = aux->getData();
-        if (student != nullptr && 
-            student->get_firstname() == first_name && 
+        if (student != nullptr &&
+            student->get_firstname() == first_name &&
             student->get_lastname() == last_name) {
             return aux;
         }
         aux = aux->getNext();
     }
 
-    cout << "Estudiante " << first_name << " " << last_name 
+    cout << "Estudiante " << first_name << " " << last_name
          << " no encontrado" << endl;
     return nullptr;
 }
@@ -199,11 +220,9 @@ bool StudentList::delete_at_position(int position) {
     }
 
     Node* aux = head;
-    Node* aux_previous = nullptr;
     int current_pos = 0;
 
     while (aux != nullptr && current_pos < position) {
-        aux_previous = aux;
         aux = aux->getNext();
         current_pos++;
     }
@@ -213,15 +232,24 @@ bool StudentList::delete_at_position(int position) {
         return false;
     }
 
-    // Actualizar punteros de navegación si es necesario
+    // Actualizar current_position si es necesario
     if (aux == current_position) {
         current_position = nullptr;
-        previous = nullptr;
-    } else if (aux == previous) {
-        previous = nullptr;
     }
 
-    aux_previous->setNext(aux->getNext());
+    // Reconectar enlaces dobles
+    if (aux->getPrev() != nullptr) {
+        aux->getPrev()->setNext(aux->getNext());
+    } else {
+        head = aux->getNext();   // era el head
+    }
+
+    if (aux->getNext() != nullptr) {
+        aux->getNext()->setPrev(aux->getPrev());
+    } else {
+        tail = aux->getPrev();   // era el tail
+    }
+
     delete aux;
     total_nodes--;
 
@@ -229,50 +257,52 @@ bool StudentList::delete_at_position(int position) {
     return true;
 }
 
-
-bool StudentList::delete_by_name(const std::string& first_name, const std::string& last_name) {
+bool StudentList::delete_by_name(const std::string& first_name,
+                                  const std::string& last_name) {
     if (head == nullptr) {
         cout << "La lista esta vacia" << endl;
         return false;
     }
 
     Node* aux = head;
-    Node* aux_previous = nullptr;
-
     while (aux != nullptr) {
         Student* student = aux->getData();
-        if (student != nullptr && 
-            student->get_firstname() == first_name && 
+        if (student != nullptr &&
+            student->get_firstname() == first_name &&
             student->get_lastname() == last_name) {
             break;
         }
-        aux_previous = aux;
         aux = aux->getNext();
     }
 
     if (aux == nullptr) {
-        cout << "Estudiante " << first_name << " " << last_name << " no encontrado" << endl;
+        cout << "Estudiante " << first_name << " " << last_name
+             << " no encontrado" << endl;
         return false;
     }
 
-    // Actualizar punteros de navegación si es necesario
     if (aux == current_position) {
         current_position = nullptr;
-        previous = nullptr;
-    } else if (aux == previous) {
-        previous = nullptr;
     }
 
-    if (aux == head) {
-        head = head->getNext();
+    // Reconectar enlaces dobles
+    if (aux->getPrev() != nullptr) {
+        aux->getPrev()->setNext(aux->getNext());
     } else {
-        aux_previous->setNext(aux->getNext());
+        head = aux->getNext();   // era el head
+    }
+
+    if (aux->getNext() != nullptr) {
+        aux->getNext()->setPrev(aux->getPrev());
+    } else {
+        tail = aux->getPrev();   // era el tail
     }
 
     delete aux;
     total_nodes--;
 
-    cout << "Estudiante " << first_name << " " << last_name << " eliminado" << endl;
+    cout << "Estudiante " << first_name << " " << last_name
+         << " eliminado" << endl;
     return true;
 }
 
@@ -285,9 +315,14 @@ bool StudentList::delete_first() {
     Node* temp = head;
     head = head->getNext();
 
+    if (head != nullptr) {
+        head->setPrev(nullptr);  // el nuevo head no tiene anterior
+    } else {
+        tail = nullptr;          // la lista quedo vacia
+    }
+
     if (temp == current_position) {
         current_position = head;
-        previous = nullptr;
     }
 
     delete temp;
@@ -307,8 +342,8 @@ void StudentList::delete_all() {
     }
 
     head = nullptr;
+    tail = nullptr;
     current_position = nullptr;
-    previous = nullptr;
     total_nodes = 0;
 }
 
@@ -322,19 +357,24 @@ void StudentList::show_all() const {
     int index = 1;
 
     cout << "\n+----------------------------------------------------+" << endl;
-    cout << "|          LISTA DE ESTUDIANTES                      |" << endl;
+    cout << "|          LISTA DE ESTUDIANTES (DOBLE ENLACE)       |" << endl;
     cout << "+----------------------------------------------------+" << endl;
 
     while (aux != nullptr) {
         Student* student = aux->getData();
         if (student != nullptr) {
             cout << "| [" << index << "]" << endl;
-            cout << "|   Nombre: " << student->get_firstname() << " " 
+            cout << "|   Nombre: " << student->get_firstname() << " "
                  << student->get_lastname() << endl;
             cout << "|   Edad: " << student->get_age() << endl;
             cout << "|   Grado: " << student->get_grade() << endl;
             cout << "|   Promedio: " << student->get_average() << endl;
-            
+            cout << "|   Prev: "
+                 << (aux->getPrev() ? aux->getPrev()->getData()->get_firstname() : "NULL")
+                 << " | Next: "
+                 << (aux->getNext() ? aux->getNext()->getData()->get_firstname() : "NULL")
+                 << endl;
+
             if (aux->getNext() != nullptr) {
                 cout << "+----------------------------------------------------+" << endl;
             }
@@ -353,7 +393,7 @@ void StudentList::show_at_position(int position) const {
         return;
     }
 
-    if (position < 0 or position > total_nodes) {
+    if (position < 0 || position >= total_nodes) {
         cout << "Posicion invalida" << endl;
         return;
     }
@@ -376,7 +416,7 @@ void StudentList::show_at_position(int position) const {
         cout << "\n+----------------------------------------------------+" << endl;
         cout << "| Estudiante en posicion " << position << endl;
         cout << "+----------------------------------------------------+" << endl;
-        cout << "| Nombre: " << student->get_firstname() << " " 
+        cout << "| Nombre: " << student->get_firstname() << " "
              << student->get_lastname() << endl;
         cout << "| Edad: " << student->get_age() << endl;
         cout << "| Grado: " << student->get_grade() << endl;
@@ -394,30 +434,17 @@ int StudentList::size() const {
 }
 
 Node* StudentList::first() {
-    if (head == nullptr) {
-        return nullptr;
-    }
-
+    if (head == nullptr) return nullptr;
     current_position = head;
-    previous = nullptr;
     return current_position;
 }
 
 Node* StudentList::last() {
-    if (head == nullptr) {
+    if (tail == nullptr) {
         cout << "Lista vacia" << endl;
         return nullptr;
     }
-
-    Node* aux = head;
-    previous = nullptr;
-
-    while (aux->getNext() != nullptr) {
-        previous = aux;
-        aux = aux->getNext();
-    }
-
-    current_position = aux;
+    current_position = tail;
     return current_position;
 }
 
@@ -426,7 +453,6 @@ Node* StudentList::get_current() {
         cout << "Posicion actual no definida" << endl;
         return nullptr;
     }
-
     return current_position;
 }
 
@@ -441,7 +467,6 @@ Node* StudentList::next_node() {
         return nullptr;
     }
 
-    previous = current_position;
     current_position = current_position->getNext();
     return current_position;
 }
@@ -452,32 +477,20 @@ Node* StudentList::previous_node() {
         return nullptr;
     }
 
-    if (current_position == head) {
+    if (current_position->getPrev() == nullptr) {
         cout << "Ya esta en el primer nodo" << endl;
         return nullptr;
     }
 
-    current_position = previous;
-
-    if (current_position == head) {
-        previous = nullptr;
-    } else {
-        Node* aux = head;
-        while (aux != nullptr && aux->getNext() != current_position) {
-            aux = aux->getNext();
-        }
-        previous = aux;
-    }
-
+    current_position = current_position->getPrev();
     return current_position;
 }
 
 void StudentList::reset_current() {
     current_position = head;
-    previous = nullptr;
 }
 
 void StudentList::inicializate() {
-    if(!is_empty())
+    if (!is_empty())
         delete_all();
 }
